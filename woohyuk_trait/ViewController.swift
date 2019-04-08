e23fbb23f46bfd81ca68d91b903155411ac2734c
dngur6344 <dngur6344@gmail.com>
2019-04-08 11:52:38 +0900

    Initial Commit

---
+++ woohyuk_trait/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  woohyuk_trait
+//
+//  Created by user on 08/04/2019.
+//  Copyright © 2019 woohyuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
